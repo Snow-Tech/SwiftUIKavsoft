273dc51f7378e2c195b08acac7b01812b24182e9
Brian Vieira <brian.vieira@ebankit.com>
2022-10-21 14:40:51 +0100

    Initial Commit

---
+++ SwiftUIKavsoft/SwiftUIKavsoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKavsoftApp.swift
+//  SwiftUIKavsoft
+//
+//  Created by Brian Michael on 21/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIKavsoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
